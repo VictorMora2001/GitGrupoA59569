2b8c74d2ef5ac350b7ebe2f378adfa9208bd5672
amigo <amigo@192.168.1.111>
2021-02-23 21:03:15 -0600

    Hola

---
+++ src/nombre/Nombre.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nombre;
+
+/**
+ *
+ * @author amigo
+ */
+public class Nombre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Victor");
+    }
+    
+}
